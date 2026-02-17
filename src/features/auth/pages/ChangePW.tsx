1f993510fd96a81d52a583c79aae134bdc558b8f
sjieys <0wonyounglee0@gmail.com>
2026-02-17 18:24:07 +0900

    Feat: 비밀번호 변경 요청 API 연결 (#14) - 지역변수 studentId 관리 useState 로직 삭제 - 비밀번호 요청 성공 후, 전역변수 phoneNum 관리 로직 추가

--- src/features/auth/pages/ChangePW.tsx
+++ src/features/auth/pages/ChangePW.tsx
@@ -1,12 +1,26 @@
 import { use, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useUserInfo } from '../../../store/userStore';
+import { patchChanePwRequset } from '../../../api/services';
 
 export default function ChangePW() {
-  const { studentNum: savedNum } = useUserInfo();
+  const { phoneNum: savedPhoneNum } = useUserInfo();
+  const [inputPhoneNum, setInputPhoneNum] = useState(savedPhoneNum || '');
 
-  const [studentId, setStudentId] = useState(savedNum || '');
-  const [phoneNum, setPhoneNum] = useState('');
+  const handleSubmit = async () => {
+    try {
+      if (!inputPhoneNum) {
+        alert('전화번호를 입력해주세요.');
+      }
+      await patchChanePwRequset();
+
+      // 전역변수 phoneNum에 지역변수 inputPhoneNum의 값 저장하기
+      const setPhoneNum = useUserInfo((state) => state.setPhoneNum);
+      setPhoneNum(inputPhoneNum);
+    } catch (error) {
+      alert('비밀번호 변경에 실패했습니다.');
+    }
+  };
 
   return (
     // flex flex-col: 세로 정렬 (LinearLayout orientation="vertical")
@@ -23,10 +37,6 @@ export default function ChangePW() {
           </label>
           <input
             type="text"
-            value={studentId}
-            onChange={(e) => {
-              setStudentId(e.target.value);
-            }}
             placeholder="학번 (ex. 2024XXXX)"
             // focus 시 보라색(#6610F2)으로 강조선이 생기도록 설정
             className="w-full p-2.5 bg-white border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#6610F2] focus:border-[#6610F2] outline-none"
@@ -40,9 +50,9 @@ export default function ChangePW() {
           </label>
           <input
             type="text"
-            value={phoneNum}
+            value={inputPhoneNum}
             onChange={(e) => {
-              const num = e.target.value;
+              setInputPhoneNum(e.target.value);
             }}
             placeholder="전화번호 (ex. 010XXXXXXXX)"
             className="w-full p-2.5 bg-white border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#6610F2] focus:border-[#6610F2] outline-none"
@@ -56,7 +66,10 @@ export default function ChangePW() {
 
         {/* 전송 버튼: 보라색(#6610F2) 테두리 버전 */}
         <div className="flex justify-center">
-          <button className="w-1/2 py-3 px-5 text-[#6610F2] border border-[#6610F2] rounded-lg hover:bg-[#6610f205] transition-colors font-semibold text-sm">
+          <button
+            className="w-1/2 py-3 px-5 text-[#6610F2] border border-[#6610F2] rounded-lg hover:bg-[#6610f205] transition-colors font-semibold text-sm"
+            onClick={handleSubmit}
+          >
             변경 SMS 전송
           </button>
         </div>
