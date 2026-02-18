f0236f9e7ad44fea804eafc41eb9f134562a2449
sjieys <0wonyounglee0@gmail.com>
2026-02-18 18:16:36 +0900

    Feat: 새로운 비밀번호 API 연결 (#14) - 새로 입력 받은 비밀번호로 API 연결 - 새로운 비밀번호 제출 이벤트 처리

--- src/features/auth/pages/ResetPW.tsx
+++ src/features/auth/pages/ResetPW.tsx
@@ -1,6 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { patchChangePW } from '../../../api/services';
 
 export default function ResetPW() {
+  const [inputPassword, setInputPassword] = useState('');
+  const handleSubmit = async () => {
+    if (!inputPassword) {
+      alert('새로운 비밀번호를 입력해주세요.');
+    }
+    // NOTE: 비밀번호 형식 지키는 로직 짤 듯?
+    patchChangePW(inputPassword);
+  };
   return (
     // flex flex-col: 세로 정렬 (LinearLayout orientation="vertical")
     // items-center: 중앙 정렬 (layout_gravity="center")
@@ -19,6 +29,7 @@ export default function ResetPW() {
             // focus 시 보라색(#6610F2)으로 강조선이 생기도록 설정
             className="w-full p-2.5 bg-white border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#6610F2] focus:border-[#6610F2] outline-none"
             placeholder="학번 (ex. 2024XXXX)"
+            onChange={(e) => setInputPassword(e.target.value)}
             // NOTE: 근데 재입력으로 확인도 안 하는 비번인데 안 보이게 하는게 맞나 싶음.
             // 눈모양 버튼 클릭해서 보였다 말았다 하는 형식으로 할까 싶음
           />
@@ -26,7 +37,10 @@ export default function ResetPW() {
 
         {/* 변경 버튼: 보라색(#6610F2) 테두리 버전 */}
         <div className="flex justify-center">
-          <button className="w-1/2 py-3 px-5 text-[#6610F2] border border-[#6610F2] rounded-lg hover:bg-[#6610f205] transition-colors font-semibold text-sm">
+          <button
+            className="w-1/2 py-3 px-5 text-[#6610F2] border border-[#6610F2] rounded-lg hover:bg-[#6610f205] transition-colors font-semibold text-sm"
+            onClick={handleSubmit}
+          >
             변경하기
           </button>
         </div>
