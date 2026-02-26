1e63fc214f60d9f4422126df82350962d2a17f61
sjieys <0wonyounglee0@gmail.com>
2026-02-26 18:17:24 +0900

    Mod: 주석 수정 및 토큰 저장 장소 변경 (#14) - 토큰 저장소를 local => session으로 변경

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // NOTE: 엔드포인트 나중에 수정
+  // NOTE: baseURL 나중에 수정
   baseURL: 'http://ssurent.com/v1',
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = sessionStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
