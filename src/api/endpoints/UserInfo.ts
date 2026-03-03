4c7a66fab9352558fe900588cb8fc236a610627e
sjieys <0wonyounglee0@gmail.com>
2026-03-03 16:53:33 +0900

    Mod: 내 정보 불러오기 api 데이터 구조 임시 조정 (#14) - 이후 merge할 때 주석 코드 풀기

--- src/api/endpoints/UserInfo.ts
+++ src/api/endpoints/UserInfo.ts
@@ -1,10 +1,15 @@
 import { apiClient } from '../Client';
 import { BaseResponseDto } from '../dto/BaseResponse.dto';
 import { UserInfoResponseDto } from '../dto/UserInfo.dto';
-export const getUserInfo = async (): Promise<
-  BaseResponseDto<UserInfoResponseDto>
-> => {
+
+export const getUserInfo = async (): Promise<UserInfoResponseDto> => {
   const res =
-    await apiClient<BaseResponseDto<UserInfoResponseDto>>('/v1/api/users');
+    /*     TODO: 나중에 주석쪽 코드로 바꿔야 함
+       await apiClient<BaseResponseDto<UserInfoResponseDto>>('/v1/api/users');
+       console.log(`api_res: ${res.data}`);
+   return res.data; */
+
+    await apiClient<UserInfoResponseDto>('/v1/api/users');
+  console.log(`api_res: ${res}`);
   return res;
 };
