6289524953c013f9a5e309ccbe5e828055a7165a
sjieys <0wonyounglee0@gmail.com>
2026-03-03 15:43:28 +0900

    Chore: 주석 삭제 (#14)

--- src/api/endpoints/VerifyCode.ts
+++ src/api/endpoints/VerifyCode.ts
@@ -1,20 +1,6 @@
 // #3 사용자가 인증번호 전송 API
-
 import { apiClient } from '../Client';
 import { BaseResponseDto } from '../dto/BaseResponse.dto';
-
-// TODO: 얘도 apiClient로 리팩토링 ㄱㄱ
-// export const postVerifyCode = async (verifyCode: string) => {
-//   try {
-//     const res = await axios.post('/auth/num', {
-//       // NOTE: 나중에 키값 변경
-//       verify_code: verifyCode,
-//     });
-//     return res;
-//   } catch (error) {
-//     alert('인증번호 전송에 실패했습니다.');
-//   }
-// };
 
 export const postVeifyCode = async (
   verifyCode: string,
