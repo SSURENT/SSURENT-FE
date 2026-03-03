f7ac6c609bf0fcd259cfdacf8e7dc56b0c6516b6
sjieys <0wonyounglee0@gmail.com>
2026-03-03 16:54:31 +0900

    Mod: 매개변수인 인증코드 객체화 및 인증코드 요청 dto파일 추가(#14)

--- src/api/endpoints/VerifyCode.ts
+++ src/api/endpoints/VerifyCode.ts
@@ -1,16 +1,17 @@
 // #3 사용자가 인증번호 전송 API
 import { apiClient } from '../Client';
 import { BaseResponseDto } from '../dto/BaseResponse.dto';
+import { VerifyCodeRequestDto } from '../dto/VerifyCode.dto';
 
 export const postVerifyCode = async (
-  verifyCode: string,
+  data: VerifyCodeRequestDto,
 ): Promise<BaseResponseDto<void>> => {
   const res = await apiClient<BaseResponseDto<void>>(
     // TODO: 이거 엔드포인트 아닐 거 같은데
     '/v1/api/auth/num',
     {
       method: 'POST',
-      body: JSON.stringify(verifyCode),
+      body: JSON.stringify(data),
     },
   );
   return res;
