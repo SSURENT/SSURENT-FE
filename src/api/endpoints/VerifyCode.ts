7f7777b5d40f7c4c43867e0abcf1390340697d15
sjieys <0wonyounglee0@gmail.com>
2026-03-25 17:49:44 +0900

    Chore: 서버 api 구조 변동에 따른 api 요청 구조 변경 (#39)

--- src/api/endpoints/VerifyCode.ts
+++ src/api/endpoints/VerifyCode.ts
@@ -6,13 +6,9 @@ import { VerifyCodeRequestDto } from '../dto/VerifyCode.dto';
 export const postVerifyCode = async (
   data: VerifyCodeRequestDto,
 ): Promise<BaseResponseDto<void>> => {
-  const res = await apiClient<BaseResponseDto<void>>(
-    // TODO: 이거 엔드포인트 아닐 거 같은데
-    '/v1/api/auth/num',
-    {
-      method: 'POST',
-      body: JSON.stringify(data),
-    },
-  );
+  const res = await apiClient<BaseResponseDto<void>>('/v1/api/auth/num', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
   return res;
 };
