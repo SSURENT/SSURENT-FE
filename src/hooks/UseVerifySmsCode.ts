b73e9fae7b3bb148a2c308df48d1bc420edf8906
sjieys <0wonyounglee0@gmail.com>
2026-03-27 12:39:52 +0900

    Feat: 인증번호 형식 검사 로직 추가 (#44)

--- src/hooks/UseVerifySmsCode.ts
+++ src/hooks/UseVerifySmsCode.ts
@@ -4,9 +4,12 @@ import { useUserInfo } from '../store/userStore';
 export const useSubmitVerifyCode = () => {
   const phoneNum: string = useUserInfo((state) => state.phoneNum);
   const handleSubmitVerifyCode = async (inputVerifyCode: string) => {
-    // NOTE: 나중에 inputVerifyCode 형식 검사 로직 짤 듯?
     if (!inputVerifyCode) {
       alert('인증코드를 입력해주세요.');
+      return;
+    }
+    if (!/^\d{6}$/.test(inputVerifyCode)) {
+      alert('인증코드 형식을 맞춰주세요.');
       return;
     }
     try {
@@ -15,7 +18,6 @@ export const useSubmitVerifyCode = () => {
         code: inputVerifyCode,
       });
       alert(`UseSubmitVerifyCode.ts_resetToken: ${res.resetToken}`);
-      // TODO: 스웨거에 에러코드 뜨면 에러처리하기
     } catch (error) {
       alert('오류가 발생했습니다. 다시 시도해주세요.');
     }
