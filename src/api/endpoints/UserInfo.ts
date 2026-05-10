dfa4292c17b96038c1ae049ad885931d77a4cea3
sjieys <0wonyounglee0@gmail.com>
2026-05-10 17:38:41 +0900

    Refactor: UserInfo api 로직 수정 (#51)

--- src/api/endpoints/UserInfo.ts
+++ src/api/endpoints/UserInfo.ts
@@ -1,9 +1,10 @@
 import { apiClient } from '../Client';
+import { BaseResponseDto } from '../dto/BaseResponse.dto';
 import { UserInfoResponseDto } from '../dto/UserInfo.dto';
 
 export const getUserInfo = async (): Promise<UserInfoResponseDto> => {
   const res =
-    await apiClient<UserInfoResponseDto>('/v1/api/users');
+    await apiClient<BaseResponseDto<UserInfoResponseDto>>('/v1/api/users');
   console.log(`api_res: ${res}`);
-  return res;
+  return res.data;
 };
